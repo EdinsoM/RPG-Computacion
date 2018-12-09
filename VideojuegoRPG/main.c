834c602661fbdcaaf01da1ee2ad495feaeaf33fe
EdinsoM <42047537+EdinsoM@users.noreply.github.com>
2018-12-08 21:04:35 -0430

    Seeccion de personajes lista
    Ahora se puede elegir hasta un maximo de 16 jugadores, se puede seleccionar entre 4 tipos distintos de personajes

--- VideojuegoRPG/main.c
+++ VideojuegoRPG/main.c
@@ -5,7 +5,7 @@
 
 typedef struct nodoL{
     int val;
-    struct nodo *sig;
+    struct nodoL *sig;
 } NodoL;
 
 typedef NodoL * Lista;
@@ -14,8 +14,9 @@ Lista newList(){
     return NULL;
 }
 
-typedef struct perso {
-    char nombre[16] ;//1
+typedef struct perso { //Creamos la estructura del personaje
+    char nombre[16];//1
+    //char tipo[32];
     int ptSalud;//1
     int ptEnergia;//1
     Pila* inventario;
@@ -27,59 +28,128 @@ typedef struct perso {
     int velocidad;
     int ptAccion;//1
     int jugador;
-
-} Perso;
+}Perso;
 
 typedef Perso* Personaje;
 
-Personaje newMototaxista(n[16],q){//q es el numero de jugador
-    Personaje g=malloc(sizeof(Perso));
-    g->nombre=nombre;
+Personaje newMototaxista(){
+    Personaje g = malloc(sizeof(Perso));
+    //g->tipo[32];
+    g->nombre[16];
     g->ptSalud=100;
     g->ptEnergia=30;
     g->ptAccion=20;
-    g->jugador=q;
     g->dano=10;
     g->rango=1;
     g->armadura=40;
+    return g;
 }
-Personaje newPolitiCorrupto(n[16],q){//q es el numero de jugador
-    Personaje g=malloc(sizeof(Perso));
-    g->nombre=nombre;
+Personaje newPolitiCorrupto(){
+    Personaje g = malloc(sizeof(Perso));
+    //g->tipo[32];
+    g->nombre[16];
     g->ptSalud=100;
     g->ptEnergia=30;
     g->ptAccion=20;
-    g->jugador=q;
+    g->dano=10;
+    g->rango=1;
+    g->armadura=40;
+    return g;
 }
-Personaje newMedicoCubano(n[16],q){//q es el numero de jugador
-    Personaje g=malloc(sizeof(Perso));
-    g->nombre=nombre;
+Personaje newMedicoCubano(){
+    Personaje g = malloc(sizeof(Perso));
+    //g->tipo[32];
+    g->nombre[16];
     g->ptSalud=100;
     g->ptEnergia=30;
     g->ptAccion=20;
-    g->jugador=q;
+    g->dano=10;
+    g->rango=1;
+    g->armadura=40;
+    return g;
+}
+Personaje newProfesor(){
+    Personaje g = malloc(sizeof(Perso));
+    //g->tipo[32];
+    g->nombre[16];
+    g->ptSalud=100;
+    g->ptEnergia=30;
+    g->ptAccion=20;
+    g->dano=10;
+    g->rango=1;
+    g->armadura=40;
+    return g;
 }
 
-Personaje newProfesor(n[16],q){//q es el numero de jugador
-    Personaje g=malloc(sizeof(Perso));
-    g->nombre=nombre;
-    g->ptSalud=100;
-    g->ptEnergia=30;
-    g->ptAccion=20;
-    g->jugador=q;
-}
+enum Efecto{
+    Ninguno, Electrificado, Incendiado, Congelado //Ninguno = 0, Electrificado = 1, Incendiado = 2, Congelado = 3;
+};
 
 typedef struct terreno{
-    personaje Personaje*;
-    efecto Efecto (enum);
-    items Lista (Item);
+    Perso* Personaje;
+    enum Efecto efecto;
+    // items Lista (Item);
 } Terreno[10][20];
+
+void seleccionPersonaje(){
+
+    Personaje Cantidad[16];
+
+    int seleccion, p, s;
+
+        for(p=0;p<16;p++){
+
+            if(p==15) printf("\nUltimo personaje (maximo: 16 jugadores)\n");
+
+            printf("\nPersonaje %d, tipo: ",p);
+            printf("\n(1) Mototaxista \n(2) Politico corrupto \n(3) Medico cubano \n(4) Profesor \n(0) Para salir\nOpcion: ");
+            scanf("%d", &seleccion);
+
+            if(seleccion == 0) break;
+
+            else if(seleccion == 1){
+                fflush(stdin);
+                Cantidad[p] = newMototaxista();
+                printf("\nNombre de tu mototaxista: ");
+                gets(Cantidad[p]->nombre);
+            }
+            else if(seleccion == 2){
+                fflush(stdin);
+                Cantidad[p] = newPolitiCorrupto();
+                printf("\nNombre politico corrupto: ");
+                gets(Cantidad[p]->nombre);
+            }
+            else if(seleccion == 3){
+                fflush(stdin);
+                Cantidad[p] = newMedicoCubano();
+                printf("\nNombre medico cubano: ");
+                gets(Cantidad[p]->nombre);
+            }
+            else if(seleccion == 4){
+                fflush(stdin);
+                Cantidad[p] = newProfesor();
+                printf("\nNombre profesor: ");
+                gets(Cantidad[p]->nombre);
+            }
+            else{
+                printf("\nSelecciona una opcion correcta");
+                p--;
+            }
+        }
+        for(int i = 0; i<p; i++){
+            printf("\nPersonaje %d: ",i);
+            puts(Cantidad[i]->nombre);
+            printf("Salud %d, ", Cantidad[i]->ptSalud);
+            printf("energia %d, ", Cantidad[i]->ptEnergia);
+            printf("accion %d, ", Cantidad[i]->ptAccion);
+            printf("dano %d, ", Cantidad[i]->dano);
+            printf("rango %d, ", Cantidad[i]->rango);
+            printf("armadura %d \n", Cantidad[i]->armadura);
+        }
+}
 
 int main()
 {
-    for(int i = 0; i<20;i++){
-        for(int j = 0; j<10;j++) printf("%d",ter[j][i]);
-    }
-    printf("Hello world\n");
+    seleccionPersonaje();
     return 0;
 }
